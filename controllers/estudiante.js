7c63bd5c18b5790da0176623f9fdb898f70a4a33
Alejandro Hernández <111544564+alejandrohernandez1998@users.noreply.github.com>
2024-02-19 23:46:51 -0300

    implementacion api

--- controllers/estudiante.js
+++ controllers/estudiante.js
@@ -1,7 +1,21 @@
 const {response} = require('express')
+const openAI = require('openai');
+
+const openAIInstance = new openAI({ apiKey: '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' });
 
 
 const ConsultaChatGPT=async(req,res=response)=>{
+    console.log("Entro");
+    // console.log(req.body.consulta);
+    const respuesta = await openAIInstance.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: "system", content: "Hablame de manera abreviada como es Rapel de Navidad en Chile." }],
+      });
+
+
+    console.log(respuesta);
+    console.log("--------------------------------");
+    console.log(respuesta.choices[0].message.content);
 
     res.json({  
         ok: true,
